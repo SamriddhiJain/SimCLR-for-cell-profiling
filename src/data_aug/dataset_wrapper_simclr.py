32e1ebf5e0266980b84c859ac129f7eae24e86e3
agorji <alli.gorji@gmail.com>
2021-02-19 19:41:58 +0100

    Migrate to albumentation and add elastic transform

--- src/data_aug/dataset_wrapper_simclr.py
+++ src/data_aug/dataset_wrapper_simclr.py
@@ -1,13 +1,17 @@
 import numpy as np
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
-import torchvision.transforms as transforms
+import torchvision.transforms
 from data_aug.custom import GaussianBlur, RandomCenterCrop
 from torchvision import datasets
 from data_aug.cell_dataset import CellDataset
 from data_aug.dataset_wrapper import DataSetWrapper
+import albumentations as transforms
+from albumentations.pytorch import ToTensorV2
+from PIL import Image
 
 np.random.seed(0)
+
 
 class DataSetWrapperSimCLR(DataSetWrapper):
 
@@ -27,24 +31,32 @@ class DataSetWrapperSimCLR(DataSetWrapper):
     def _get_simclr_pipeline_transform(self):
         # get a set of data augmentation transformations as described in the SimCLR paper.
         color_jitter = transforms.ColorJitter(0.8 * self.s, 0.8 * self.s, 0.8 * self.s, 0.2 * self.s)
-        data_transforms = transforms.Compose([transforms.RandomResizedCrop(size=self.input_shape[0]),
-                                              transforms.RandomHorizontalFlip(),
-                                              transforms.RandomApply([color_jitter], p=0.8),
+        im_w = self.input_shape[0]
+        data_transforms = transforms.Compose([transforms.RandomResizedCrop(im_w, im_w),
+                                              # transforms.Compose([RandomCenterCrop(),
+                                              #                    transforms.Resize(size=self.input_shape[0])]),
+                                              transforms.HorizontalFlip(),
+                                              transforms.ElasticTransform(),
+                                              # transforms.RandomRotate90(),
+                                              transforms.ColorJitter(p=0.8),
                                               # transforms.RandomGrayscale(p=0.2),
                                               # transforms.Compose([RandomCenterCrop(),
                                               #                     transforms.Resize(size=self.input_shape[0])]),
                                               # transforms.Compose([transforms.CenterCrop(self.input_shape[0]//2),
                                               #                    transforms.Resize(size=self.input_shape[0])]),
-                                              GaussianBlur(kernel_size=int(0.1 * self.input_shape[0])),
-                                              transforms.ToTensor(),
-                                              transforms.Normalize((0.7469, 0.7403, 0.7307), (0.1548, 0.1594, 0.1706))])
+                                              transforms.GaussianBlur(blur_limit=[int(0.1 * im_w), int(0.1 * im_w)],
+                                                                      sigma_limit=[0.1, 2]),
+                                              transforms.Normalize((0.7469, 0.7403, 0.7307), (0.1548, 0.1594, 0.1706)),
+                                              ToTensorV2()
+                                              ])
         return data_transforms
+
 
 class SimCLRDataTransform(object):
     def __init__(self, transform):
         self.transform = transform
 
     def __call__(self, sample):
-        xi = self.transform(sample)
-        xj = self.transform(sample)
+        xi = self.transform(image=np.array(sample))["image"]
+        xj = self.transform(image=np.array(sample))["image"]
         return xi, xj
